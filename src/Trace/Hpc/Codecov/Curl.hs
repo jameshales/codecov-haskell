9abe55c3a0d7cea9ff66dfbe6a8cfb0228938c20
James Hales <james@jameshales.org>
2017-02-18 13:16:32 +1100

    Remove redundant Control.Applicative imports

--- src/Trace/Hpc/Codecov/Curl.hs
+++ src/Trace/Hpc/Codecov/Curl.hs
@@ -12,7 +12,6 @@
 
 module Trace.Hpc.Codecov.Curl ( postJson, readCoverageResult, PostResult (..) ) where
 
-import           Control.Applicative
 import           Control.Monad
 import           Data.Aeson
 import           Data.Aeson.Types (parseMaybe)
